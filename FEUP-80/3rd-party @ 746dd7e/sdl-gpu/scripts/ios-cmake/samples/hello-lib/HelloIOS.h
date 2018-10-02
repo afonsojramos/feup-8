ebdc93776809d9924946a675059a33e1be53ab5c
Nádia Carvalho <nadiacarvalho118@gmail.com>
2018-10-02 13:38:30 +0100

    Deleted 3rd-party @ 76...

--- FEUP-80/3rd-party @ 746dd7e/sdl-gpu/scripts/ios-cmake/samples/hello-lib/HelloIOS.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface HelloIOS:UIViewController
-
-- (NSString*)getHello;
-
-@end
--- FEUP-80/3rd-party @ 746dd7e/wren-0.1.0/test/language/module/module_dir/module_dir.wren
+++
@@ -1,3 +0,0 @@
-import "something" for Index
-
-System.print(Index) // expect: index
