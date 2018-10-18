2f68ccc94b50fd60d45bcd4b5ca9190e1fec7d76
up201503995 <up201503995@fe.up.pt>
2018-10-18 22:41:53 +0100

    continued saveExerciseProgress method

--- Website/app/Http/Controllers/API/ExerciseController.php
+++ Website/app/Http/Controllers/API/ExerciseController.php
@@ -22,12 +22,13 @@ class ExerciseController extends Controller
         try 
         {
             $exercises = DB::table('exercise')->select('id', 'title')->where('isPrivate', 'false')->get();
-            return response()->json(['response_code'=>0, 'exercises'=> $exercises], 200);
         } 
         catch (\Exception $e) 
         {
             return response()->json(['response_code'=>1], 200);
         }
+
+        return response()->json(['response_code'=>0, 'exercises'=> $exercises], 200);
     }
 
 
@@ -47,27 +48,90 @@ class ExerciseController extends Controller
             ->select('exercise.title', 'exercise.description', 'exercise.image_path as image_base64',
                  'test.correct_code as correct_code_base64' , 'test.tests_code as test_code_base64', 'users.name as creator_name')
             ->where('exercise.id', '=', $id)->get();
+            //TODO: send feup8 file
             //TODO: send image then
             //TODO: ->join('exerciseStudent', 'exercise.id', '=', 'exerciseStudent.exercise_id') mas com exerciseStudent.student_id = Auth::id indo buscar o 'exerciseStudent.progress' no select
-            return response()->json(['response_code'=>0, 'exercise'=> $exercise], 200);
         } 
         catch (\Exception $e) 
         {
             return response()->json(['response_code'=>1], 200);
         }
+
+        return response()->json(['response_code'=>0, 'exercise'=> $exercise], 200);
     }
 
     /** 
      * This function will save the exercise progress of a student.
-     * @param $id The id of the exercise of which progress will be saved.
+     * @param $request The received request with the exercise data, the tic file of the development.
+     * @param $exercise_id The id of the exercise of which progress will be saved.
      * @return the response code 0 indicating sucess. Response code 1 indicating that must be logged in to save progress. Response code 2 for other errors.
     */ 
-    public function saveExerciseProgress(Request $request, $id)
+    public function saveExerciseProgress(Request $request, $exercise_id)
     { 
         if(!Auth::check())
             return response()->json(['responde_code'=>1], 200); //must be logged in to save progress
 
-        
+        // $progress = correr a funçao que vai executar os testes para saber o progress, $request['code']
+        try 
+        {
+            if(checkExerciseStudentExists())
+            {
+                DB::table('exerciseStudent')
+                    ->where('exercise_id', '=', $exercise_id)
+                    ->where('student_id', '=', Auth::user()->id)
+                    ->update(['progress'=>$progress])
+                    ->update(['feup8_file'=>$request['exercise_data']]);
+            }
+            else
+            {
+                DB::table('exerciseStudent')->insert(['exercise_id' => $exercise_id, 'student_id' => Auth::user()->id, 
+                    'progress'=>$progress, 'feup8_file'=>$request['exercise_data']]);
+            }    
+        } 
+        catch (\Exception $e) 
+        {
+            return response()->json(['response_code'=>1], 200);
+        }
+
+        return response()->json(['response_code'=>0], 200);
+    }
+
+
+    /** 
+     * This function will check if an entry on database for ExerciseStudent with the auth user id and the exercise id given.     
+     * @param exercise_id The id of the exercise to be checked
+     * @return bool true if exists, false otherwise
+    */ 
+    public static function checkExerciseStudentExists($exercise_id)
+    { 
+        try 
+        {
+            $exercise_student = DB::table('exerciseStudent')
+                ->select('*')
+                ->where('exercise_id', '=', $exercise_id)
+                ->where('student_id', '=', Auth::id)
+                ->get();
+            if(count($exercise_student) != 0)
+                return true;
+        } 
+        catch (\Exception $e) 
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+
+     /** 
+     * This function will save the exercise progress of a student.
+     * @param $request The received request with the exercise code.
+     * @param $exercise_id The id of the exercise of which student code will be tested.
+     * @return the response code 0 indicating sucess. Response code 1 indicating that must be logged in to save progress. Response code 2 for other errors.
+    */ 
+    public function testStudentCodeForExercise(Request $request, $exercise_id)
+    { 
+       
     }
 
 }
