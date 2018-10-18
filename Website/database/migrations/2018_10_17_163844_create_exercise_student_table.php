4604cd676410fcf25a856e83b5ed3b0bbca14281
NadiaCarvalho <nadiacarvalho118@gmail.com>
2018-10-18 21:36:38 +0000

    Feature database migrations

---
+++ Website/database/migrations/2018_10_17_163844_create_exercise_student_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExerciseStudentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ExerciseStudent', function (Blueprint $table) {
+            $table->integer('student_id');
+            $table->foreign('student_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('exercise_id');
+            $table->foreign('exercise_id')->references('id')->on('Exercise')->onDelete('cascade');
+            $table->integer('progress');
+            $table->binary('feup8_file');
+            $table->primary(['student_id', 'exercise_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ExerciseStudent');
+    }
+}
