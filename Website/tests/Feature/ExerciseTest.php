2ac4141ddcc9f7637efd6244b18a088c8581fe17
Dinis Trigo <up201504196@fe.up.pt>
2018-10-22 18:36:51 +0100

    tests for getting exercise details done

--- Website/tests/Feature/ExerciseTest.php
+++ Website/tests/Feature/ExerciseTest.php
@@ -57,17 +57,27 @@ class ExerciseTest extends TestCase
         $this->assertFalse(array_key_exists('exercises', $response_array));*/
         echo $response->decodeResponseJson();
         array_diff($response->decodeResponseJson(), json_decode(
-        {
+        `{
             "response_code": 0,
-            "exercise":
-            [
+            "exercise": [
                 {
-                    "id": 1,
-                    "title": "Make a supermario game.",
-                    "progress": 0
+                    "title": "sit amet metus.",
+                    "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et arcu imperdiet ullamcorper. Duis at lacus. Quisque purus sapien, gravida non, sollicitudin a, malesuada id, erat.",
+                    "image_base64": "1.png",
+                    "creator_name": "Utilizador Um",
+                    "progress": "0",
+                    "feup8_file": ""
+                }
+            ],
+            "tests": [
+                {
+                    "id": "1",
+                    "title": "Duis elementum,",
+                    "test_code": "Suspendisse sed dolor. Fusce mi lorem, vehicula et, rutrum eu, ultrices sit amet, risus. Donec",
+                    "hint": "ac, fermentum vel, mauris. Integer sem elit, pharetra ut,"
                 }
             ]
-        }));
+        }`));
         
     }
 
