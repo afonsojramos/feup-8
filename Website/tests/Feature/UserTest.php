f342ba78527895c8b683e6df22dbd00a9b8b05e9
Davide Costa <up201503995@fe.up.pt>
2018-10-22 18:18:36 +0100

    Added files of testing UserController and ExerciseController

---
+++ Website/tests/Feature/UserTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserTests extends TestCase
+{
+    /**
+     * This
+     *
+     */
+    public function testRegister()
+    {
+        $response = $this->call('POST', '/api/register', ['username' => 'user1', 'password' => 'password', 
+            'name' => 'name', 'email' => 'email']);
+        $response->assertStatus(200);
+        $response_array = $response->decodeResponseJson();
+        $this->assertEquals($response_array['response_code'], 0);
+        $this->assertTrue(array_key_exists('auth_token', $response_array));
+    }
+
+        /**
+     * This
+     *
+     */
+    public function testRegisterRepeated()
+    {
+        $response = $this->call('POST', '/api/register', ['username' => 'user1', 'password' => 'password', 
+            'name' => 'name', 'email' => 'email']);
+        $response->assertStatus(200);
+        $response_array = $response->decodeResponseJson();
+        $this->assertEquals($response_array['response_code'], 1);
+        $this->assertFalse(array_key_exists('auth_token', $response_array));
+    }
+
+    /**
+     * A basic test example.
+     *
+     */
+    public function testLogin()
+    {
+        $response = $this->call('POST', '/api/login', ['username' => 'user1', 'password' => '$2y$10$vOp6qnGv9Ae/0NdmD/D7fudXzoU6CltbYSSrIqK2/ARmTUBgzo2gK']);
+        $response->assertStatus(200);
+        $response_array = $response->decodeResponseJson();
+        $this->assertEquals($response_array['response_code'], 0);
+        $this->assertTrue(array_key_exists('auth_token', $response_array));
+    }
+}
