5610e2703d350f0ddef8a7b00ea126d46199cbc2
up201503995 <up201503995@fe.up.pt>
2018-10-27 17:09:42 +0100

    Added methods for logout test

--- Website/tests/Feature/UserTest.php
+++ Website/tests/Feature/UserTest.php
@@ -14,7 +14,7 @@ class UserTests extends TestCase
     {
         $input = ['username' => 'user1', 'password' => 'password', 
         'name' => 'name'];
-        testRegister($input, 1, false);
+        testRegister($input, 1, false, 1);
     }
 
     /**
@@ -24,7 +24,7 @@ class UserTests extends TestCase
     {
         $input = ['username' => 'user1', 'password' => 'password', 
         'email' => 'email'];
-        testRegister($input, 1, false);
+        testRegister($input, 1, false, 1);
     }
 
     /**
@@ -34,7 +34,7 @@ class UserTests extends TestCase
     {
         $input = ['username' => 'user1', 'name' => 'name', 
         'email' => 'email'];
-        testRegister($input, 1, false);
+        testRegister($input, 1, false, 1);
     }   
     
     /**
@@ -44,7 +44,7 @@ class UserTests extends TestCase
     {
         $input = ['password' => 'password', 'name' => 'name', 
         'email' => 'email'];
-        testRegister($input, 1, false);
+        testRegister($input, 1, false, 1);
     }     
 
     /**
@@ -54,7 +54,7 @@ class UserTests extends TestCase
     {
         $input = ['username' => 'user1', 'password' => 'password', 
         'name' => 'name', 'email' => 'email'];
-        testRegister($input, 0, true);
+        testRegister($input, 0, true, 2);
     }
 
     /**
@@ -62,26 +62,40 @@ class UserTests extends TestCase
      * It should be called with data that either causes success or not and the expected returns accordingly.
      * It is used to test success and failure avoiding code duplication.
      */
-    public function testRegister($input, $expected_response_code, $should_succeed)
+    public function testRegister($input, $expected_response_code, $should_succeed, $numberOfElementsReceived)
     {
         $response = $this->call('POST', '/api/register', $input);
         $response->assertStatus(200);
         $response_array = $response->decodeResponseJson();
         $this->assertEquals($response_array['response_code'], $expected_response_code);
         $this->assertEquals(array_key_exists('auth_token', $response_array), $should_succeed);
+        $this->assertEquals(count($response_array), $numberOfElementsReceived);
     }
 
-    /**
-     * Tests if a repeated register is rejected. (It should fail because the username or email are already in the db).
+     /**
+     * Tests if a repeated register is rejected. (It should fail because the username are already in the db).
      */
-    public function testRegisterRepeated()
+    public function testRegisterRepeatedUsername()
     {
         $response = $this->call('POST', '/api/register', ['username' => 'user1', 'password' => 'password', 
             'name' => 'name', 'email' => 'email']);
         $response->assertStatus(200);
         $response_array = $response->decodeResponseJson();
         $this->assertEquals($response_array['response_code'], 1);
-        $this->assertFalse(array_key_exists('auth_token', $response_array));
+        $this->assertTrue(count($response_array) == 1);
+    }
+
+    /**
+     * Tests if a repeated register for email is rejected. (It should fail because the email are already in the db).
+     */
+    public function testRegisterRepeatedEmail()
+    {
+        $response = $this->call('POST', '/api/register', ['username' => 'user2', 'password' => 'password', 
+            'name' => 'name', 'email' => 'email']);
+        $response->assertStatus(200);
+        $response_array = $response->decodeResponseJson();
+        $this->assertEquals($response_array['response_code'], 2);
+        $this->assertTrue(count($response_array) == 1);
     }
 
     /**
@@ -90,7 +104,7 @@ class UserTests extends TestCase
     public function testLoginFailForWrongPassword()
     {
         $input = ['username' => 'user1', 'password' => 'passwordWrong'];
-        testLogin($input, 1, false);
+        testLogin($input, 1, false, 1);
     }
 
     /**
@@ -99,7 +113,7 @@ class UserTests extends TestCase
     public function testLoginFailForInexistentUsername()
     {
         $input = ['username' => 'userNotExistent', 'password' => 'password'];
-        testLogin($input, 1, false);
+        testLogin($input, 1, false, 1);
     }
 
     /**
@@ -108,7 +122,7 @@ class UserTests extends TestCase
     public function testLoginCorrectly()
     {
         $input = ['username' => 'user1', 'password' => '$2y$10$vOp6qnGv9Ae/0NdmD/D7fudXzoU6CltbYSSrIqK2/ARmTUBgzo2gK'];
-        testLogin($input, 0, true);
+        testLogin($input, 0, true, 2);
     }
 
     /**
@@ -116,12 +130,38 @@ class UserTests extends TestCase
      * It should be called with data that either causes success or not and the expected returns accordingly.
      * It is used to test success and failure avoiding code duplication.
      * */
-    public function testLogin($input, $expected_response_code, $should_succeed)
+    public function testLogin($input, $expected_response_code, $should_succeed, $numberOfElementsReceived)
     {
         $response = $this->call('POST', '/api/login', $input);
         $response->assertStatus(200);
         $response_array = $response->decodeResponseJson();
         $this->assertEquals($response_array['response_code'], $expected_response_code);
         $this->assertEquals(array_key_exists('auth_token', $response_array), $should_succeed);
+        $this->assertEquals(count($response_array), $numberOfElementsReceived);
+    }
+
+    /**
+     * Tests if a logout is rejected if user not logged in
+     */
+    public function testLogoutWithoutBeingLoggedIn()
+    {
+        $response = $this->call('POST', '/api/logout');
+        $response->assertStatus(200);
+        $response_array = $response->decodeResponseJson();
+        $this->assertEquals($response_array['response_code'], 1);
+        $this->assertTrue(count($response_array) == 1);
+    }
+   
+    /**
+     * Tests if a successfull logout can be done, all the necessary parameters are given.
+     */
+    public function testLogoutSuccessfully()
+    {
+        $loginResponse = $this->call('POST', '/api/login', ['username' => 'user1', 'password' => '$2y$10$vOp6qnGv9Ae/0NdmD/D7fudXzoU6CltbYSSrIqK2/ARmTUBgzo2gK']);
+        $response = $this->call('POST', '/api/logout', ['auth_token' => $loginResponse['auth_token']]);
+        $response->assertStatus(200);
+        $response_array = $response->decodeResponseJson();
+        $this->assertEquals($response_array['response_code'], 0);
+        $this->assertTrue(count($response_array) == 1);
     }
 }
