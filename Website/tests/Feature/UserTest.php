ed9f680b909bd5e656a5847868bd5f5fd8b5b1cf
up201503995 <up201503995@fe.up.pt>
2018-10-27 21:04:48 +0100

    improved tests for UserTest according to tests db

--- Website/tests/Feature/UserTest.php
+++ Website/tests/Feature/UserTest.php
@@ -7,56 +7,10 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UserTests extends TestCase
 {
-    /**
-     * Tests if a register without email is rejected.
-     */
-    public function testRegisterWithoutEmail()
-    {
-        $input = ['username' => 'user1', 'password' => 'password', 
-        'name' => 'name'];
-        testRegister($input, 1, false, 1);
-    }
+    use RefreshDatabase;
 
-    /**
-     * Tests if a register without name is rejected.
-     */
-    public function testRegisterWithoutName()
-    {
-        $input = ['username' => 'user1', 'password' => 'password', 
-        'email' => 'email'];
-        testRegister($input, 1, false, 1);
-    }
 
-    /**
-     * Tests if a register without password is rejected.
-     */
-    public function testRegisterWithoutPassword()
-    {
-        $input = ['username' => 'user1', 'name' => 'name', 
-        'email' => 'email'];
-        testRegister($input, 1, false, 1);
-    }   
-    
-    /**
-     * Tests if a register without username is rejected.
-     */
-    public function testRegisterWithoutUserName()
-    {
-        $input = ['password' => 'password', 'name' => 'name', 
-        'email' => 'email'];
-        testRegister($input, 1, false, 1);
-    }     
-
-    /**
-     * Tests if a successfull register can be done, all the necessary parameters are given, and for the first time(not repeated username or email).
-     */
-    public function testRegisterCorrectly()
-    {
-        $input = ['username' => 'user1', 'password' => 'password', 
-        'name' => 'name', 'email' => 'email'];
-        testRegister($input, 0, true, 2);
-    }
-
+    /* Register tests
     /**
      * Generic method used for testing register feature.
      * It should be called with data that either causes success or not and the expected returns accordingly.
@@ -72,17 +26,67 @@ class UserTests extends TestCase
         $this->assertEquals(count($response_array), $numberOfElementsReceived);
     }
 
-     /**
+    /**
+     * Tests if a register without email is rejected.
+     */
+    public function testRegisterWithoutEmail()
+    {
+        $input = ['username' => 'user_register', 'password' => 'password_register', 
+        'name' => 'name_register'];
+        testRegister($input, 1, false, 1);
+    }
+
+    /**
+     * Tests if a register without name is rejected.
+     */
+    public function testRegisterWithoutName()
+    {
+        $input = ['username' => 'user_register', 'password' => 'password_register', 
+        'email' => 'email_register'];
+        testRegister($input, 1, false, 1);
+    }
+
+    /**
+     * Tests if a register without password is rejected.
+     */
+    public function testRegisterWithoutPassword()
+    {
+        $input = ['username' => 'user_register', 'name' => 'name_register', 
+        'email' => 'email_register'];
+        testRegister($input, 1, false, 1);
+    }   
+    
+    /**
+     * Tests if a register without username is rejected.
+     */
+    public function testRegisterWithoutUserName()
+    {
+        $input = ['password' => 'password_register', 'name' => 'name_register', 
+        'email' => 'email_register'];
+        testRegister($input, 1, false, 1);
+    }     
+
+    /**
+     * Tests if a successfull register can be done, all the necessary parameters are given, and for the first time(not repeated username or email).
+     */
+    public function testRegisterCorrectly()
+    {
+        $input = ['username' => 'user_register', 'password' => 'password_register', 
+        'name' => 'name_register', 'email' => 'email_register'];
+        testRegister($input, 0, true, 2);
+    }
+
+    /**
      * Tests if a repeated register is rejected. (It should fail because the username are already in the db).
      */
     public function testRegisterRepeatedUsername()
     {
-        $response = $this->call('POST', '/api/register', ['username' => 'user1', 'password' => 'password', 
-            'name' => 'name', 'email' => 'email']);
-        $response->assertStatus(200);
-        $response_array = $response->decodeResponseJson();
-        $this->assertEquals($response_array['response_code'], 1);
-        $this->assertTrue(count($response_array) == 1);
+        $input_1 = ['username' => 'user_register_repeat', 'password' => 'password_register_repeat', 
+            'name' => 'name_register_repeat', 'email' => 'email_register_repeat'];
+        $input_2 = ['username' => 'user_register_repeat', 'password' => 'password_register_repeat', 
+            'name' => 'name_register_repeat', 'email' => 'email_register_different'];
+        testRegister($input, 0, true, 2);
+        testRegister($input, 1, false, 1);
     }
 
     /**
@@ -90,41 +94,17 @@ class UserTests extends TestCase
      */
     public function testRegisterRepeatedEmail()
     {
-        $response = $this->call('POST', '/api/register', ['username' => 'user2', 'password' => 'password', 
-            'name' => 'name', 'email' => 'email']);
-        $response->assertStatus(200);
-        $response_array = $response->decodeResponseJson();
-        $this->assertEquals($response_array['response_code'], 2);
-        $this->assertTrue(count($response_array) == 1);
+        $input_1 = ['username' => 'user_register_repeat', 'password' => 'password_register_repeat', 
+            'name' => 'name_register_repeat', 'email' => 'email_register_repeat'];
+        $input_2 = ['username' => 'user_register_different', 'password' => 'password_register_repeat', 
+            'name' => 'name_register_repeat', 'email' => 'email_register_repeat'];
+        testRegister($input, 0, true, 2);
+        testRegister($input, 1, false, 1);
     }
 
-    /**
-     * Tests if login fails with wrong password for given username.
-     */
-    public function testLoginFailForWrongPassword()
-    {
-        $input = ['username' => 'user1', 'password' => 'passwordWrong'];
-        testLogin($input, 1, false, 1);
-    }
 
-    /**
-     * Tests if login fails with inexistent username.
-     */
-    public function testLoginFailForInexistentUsername()
-    {
-        $input = ['username' => 'userNotExistent', 'password' => 'password'];
-        testLogin($input, 1, false, 1);
-    }
 
-    /**
-     * Tests if a successfull login can be done, all the necessary parameters are given.
-     */
-    public function testLoginCorrectly()
-    {
-        $input = ['username' => 'user1', 'password' => '$2y$10$vOp6qnGv9Ae/0NdmD/D7fudXzoU6CltbYSSrIqK2/ARmTUBgzo2gK'];
-        testLogin($input, 0, true, 2);
-    }
-
+    /* Login tests
     /**
      * Generic method used for testing login feature.
      * It should be called with data that either causes success or not and the expected returns accordingly.
@@ -141,15 +121,56 @@ class UserTests extends TestCase
     }
 
     /**
+     * Tests if login fails with wrong password for given username.
+     */
+    public function testLoginFailForWrongPassword()
+    {
+        $input = ['username' => 'user_already_in_db', 'password' => 'wrong_password'];
+        testLogin($input, 1, false, 1);
+    }
+
+    /**
+     * Tests if login fails with inexistent username.
+     */
+    public function testLoginFailForInexistentUsername()
+    {
+        $input = ['username' => 'user_not_in_db', 'password' => 'password'];
+        testLogin($input, 1, false, 1);
+    }
+
+    /**
+     * Tests if a successfull login can be done, all the necessary parameters are given.
+     */
+    public function testLoginCorrectly()
+    {
+        $input = ['username' => 'user_already_in_db', 'password' => 'password_already_in_db'];
+        testLogin($input, 0, true, 2);
+    }
+
+    
+
+    // Logout Tests
+    /**
+     * Generic method used for testing logout feature.
+     * It should be called with data that either causes success or not and the expected returns accordingly.
+     * It is used to test success and failure avoiding code duplication.
+     * */
+    public function testLogout($input, $expected_response_code, $numberOfElementsReceived)
+    {
+        $response = $this->call('POST', '/api/logout', $input);
+        $response->assertStatus(200);
+        $response_array = $response->decodeResponseJson();
+        $this->assertEquals($response_array['response_code'], $expected_response_code);
+        $this->assertEquals(count($response_array), $numberOfElementsReceived);
+    }
+
+    /**
      * Tests if a logout is rejected if user not logged in
      */
     public function testLogoutWithoutBeingLoggedIn()
     {
-        $response = $this->call('POST', '/api/logout');
-        $response->assertStatus(200);
-        $response_array = $response->decodeResponseJson();
-        $this->assertEquals($response_array['response_code'], 1);
-        $this->assertTrue(count($response_array) == 1);
+        $input = [];
+        testLogout($input, 1, 1);
     }
    
     /**
@@ -157,11 +178,9 @@ class UserTests extends TestCase
      */
     public function testLogoutSuccessfully()
     {
-        $loginResponse = $this->call('POST', '/api/login', ['username' => 'user1', 'password' => '$2y$10$vOp6qnGv9Ae/0NdmD/D7fudXzoU6CltbYSSrIqK2/ARmTUBgzo2gK']);
-        $response = $this->call('POST', '/api/logout', ['auth_token' => $loginResponse['auth_token']]);
-        $response->assertStatus(200);
-        $response_array = $response->decodeResponseJson();
-        $this->assertEquals($response_array['response_code'], 0);
-        $this->assertTrue(count($response_array) == 1);
+        $login_response = $this->call('POST', '/api/login', ['username' => 'user_already_in_db', 'password' => 'password_already_in_db']);
+        $login_response_array = $login_response->decodeResponseJson();
+        $input = ['auth_token' => $login_response_array['auth_token']];
+        testLogout($input, 0, 1);
     }
 }
