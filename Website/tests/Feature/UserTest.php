b3826a30375dae786cc85a98101bb26058689f9a
up201503995 <up201503995@fe.up.pt>
2018-10-27 22:04:46 +0100

    Improved testLoginCorrectly()

--- Website/tests/Feature/UserTest.php
+++ Website/tests/Feature/UserTest.php
@@ -143,6 +143,8 @@ class UserTests extends TestCase
      */
     public function testLoginCorrectly()
     {
+        $response = $this->call('POST', '/api/register', ['username' => 'user_already_in_db', 
+            'password' => 'password_already_in_db', 'name' => 'name', 'email' => 'email_already_in_db']);
         $input = ['username' => 'user_already_in_db', 'password' => 'password_already_in_db'];
         $this->genericTestLogin($input, 0, true, 2);
     }
