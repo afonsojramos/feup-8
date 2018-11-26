7836498b545b28c2d06945aa12ea8b206261a443
Dinis Trigo <up201504196@fe.up.pt>
2018-11-26 14:26:50 +0000

    Improve coverage in feup 8

--- FEUP-8/src/unit_tests.h
+++ FEUP-8/src/unit_tests.h
@@ -1,3 +1,15 @@
 #pragma once
 
+
+void testExample();
+void testNet();
+void testLoginRequest();
+void testRegisterRequest();
+void testLogoutRequest();
+void testGetExercisesListRequest();
+void testGetExerciseDetailsRequest();
+void testSaveProgressRequest();
+void testSendCodeToServerAndGetTestsResultsRequestSend();
+void testSetAllTestsAsValue();
+void testParseExerciseTestsReceived();
 int run_test_suite();
