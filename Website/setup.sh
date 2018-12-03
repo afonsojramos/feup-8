59aff8da36f30c3599db41543fcef67f26c50cbf
Rui Leixo <ruileixo@hotmail.com>
2018-12-03 17:15:03 +0000

    Database Permissions and Login problem

--- Website/setup.sh
+++ Website/setup.sh
@@ -3,6 +3,8 @@
 sed -i 's?DB_DATABASE=.*$?DB_DATABASE='`pwd`'/database/database_tests.sqlite?' .env
 
 touch database/database_tests.sqlite
+chmod 0777 database/database_tests.sqlite
+
 composer install
 php artisan migrate --seed
 php artisan passport:install --force
