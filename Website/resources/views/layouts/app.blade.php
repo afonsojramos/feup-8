2ae6e9cec9f8c43c33bb8d0fd1360673a30c171e
Afonso Ramos <up201506239@fe.up.pt>
2018-12-06 17:38:15 +0000

    New Fonts

--- Website/resources/views/layouts/app.blade.php
+++ Website/resources/views/layouts/app.blade.php
@@ -17,7 +17,8 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
